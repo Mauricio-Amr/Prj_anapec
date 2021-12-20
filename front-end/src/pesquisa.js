623874e6d732ac0bc310df7f2c4fb64edac5167f
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-12-20 15:06:31 -0300

    correção para iniciar a pesquisa
    criando script js pesquisa

--- front-end/src/pesquisa.js
+++ front-end/src/pesquisa.js
@@ -1,9 +1,19 @@
-const formulario = document.getElementById('peca').value
+ 
+ 
+ function enviar(e){
+  
+  const valor = document.getElementById('fm-pesq').value
 
-function enviar(){
+  const body = document.querySelector('body')
+  const div = document.querySelector('div')
+  const paragrafo = document.createElement('p')
 
+  paragrafo.innerHTML = valor
 
+  const div1 = document.getElementById('div1')
 
+  div1.appendChild(paragrafo)
+  
+  
 
-    alert("formedata.get('peca')")
 }
