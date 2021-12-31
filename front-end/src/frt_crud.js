add2cc63216f91cbf42c50ad367c5af106af2f89
Mauricio-Amr <mauricio.s.amr@gmail.com>
2021-12-31 15:58:49 -0300

    iniciando css

--- front-end/src/frt_crud.js
+++ front-end/src/frt_crud.js
@@ -10,122 +10,125 @@ const form = new FormData()
 
 function enviar() {
 
-    const valor = document.getElementById('fm-pesq').value
+    const valor = document.getElementById('fm-pesq-imp').value
     if (document.getElementById('tab-res-pes')) {
         document.getElementById('tab-res-pes').remove()
         getPecas(valor)
     }
     else {
-    getPecas(valor)
+        getPecas(valor)
     }
 }
 
 async function getPecas(codigo) {
 
+    //Criando a tabela de resultado  da pesquuisa
+    let tabela = document.createElement('table')
+    tabela.setAttribute('id', 'tab-res-pes');
 
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
 
-    
-        let table = document.createElement('table')
-        table.setAttribute('id', 'tab-res-pes');
+    tabela.appendChild(thead);
+    tabela.appendChild(tbody);
 
-        let thead = document.createElement('thead');
-        let tbody = document.createElement('tbody');
-
-        table.appendChild(thead);
-        table.appendChild(tbody);
-
-        // Adding the entire table to the body tag
-        document.getElementById('lspeca').appendChild(table);
-
-        // Creating and adding data to first row of the table
-        let row_1 = document.createElement('tr');
-        let heading_1 = document.createElement('th');
-        heading_1.innerHTML = "CODIGO";
-        let heading_2 = document.createElement('th');
-        heading_2.innerHTML = "NOME";
-
-        row_1.appendChild(heading_1);
-        row_1.appendChild(heading_2);
-        thead.appendChild(row_1);
-
-
-       
-            const url = `http://localhost:3000/api/pecas/${codigo}`
-
-            return await axios.get(url)
-                .then(response => {
-
-
-                    let data = response.data.result
-
-                    console.log(data)
-                    //console.log(data[0].codigo)
-
-
-                    for (var i = 0; i < data.length; i++) {
-                        // for(let k in data){
-                        // let valor =  data[k]
-                        // console.log(valor)
-                        let codigo = data[i].codigo
-                        let nome = data[i].nome
-
-                        let row_2 = document.createElement('tr');
-                        let row_2_data_1 = document.createElement('td');
-                        row_2_data_1.innerHTML = codigo;
-                        let row_2_data_2 = document.createElement('td');
-                        row_2_data_2.innerHTML = nome;
-
-                        row_2.appendChild(row_2_data_1);
-                        row_2.appendChild(row_2_data_2);
-                        tbody.appendChild(row_2);
-                    }
-                }
-        //}
+   //adicionando a tabela a div
+    document.getElementById('lspeca').appendChild(tabela);
 
     
+    //Criando e adicionando o  cabeçalho da tabela
+    let linha_1 = document.createElement('tr');
+    let coluna_1 = document.createElement('th');
+    coluna_1.innerHTML = "CODIGO COMPATIVEL";
+    let coluna_2 = document.createElement('th');
+    coluna_2.innerHTML = "NOME";
+    let coluna_3 = document.createElement('th')
+    coluna_3.innerHTML ="MODELO"
+
+
+    linha_1.appendChild(coluna_1);
+    linha_1.appendChild(coluna_2);
+    linha_1.appendChild(coluna_3)
+    thead.appendChild(linha_1);
+
+
+
+    const url = `http://localhost:3000/api/pecas/${codigo}`
+
+    return await axios.get(url)
+        .then(response => {
+
+
+            let data = response.data.result
+
+            console.log(data)
+            //console.log(data[0].codigo)
+
+
+            for (var i = 0; i < data.length; i++) {
+                //adiciona o resultado ao  corpo da tabela
+                let codigo_comp = data[i].codigo
+                let nome = data[i].nome
+                let modelo =data[i].modelo
+
+                let linha_2 = document.createElement('tr');
+                let linha_2_data_1 = document.createElement('td');
+                linha_2_data_1.innerHTML = codigo_comp;
+                let linha_2_data_2 = document.createElement('td');
+                linha_2_data_2.innerHTML = nome;
+                let linha_2_data_3 = document.createElement('td')
+                linha_2_data_3.innerHTML = modelo
+
+                linha_2.appendChild(linha_2_data_1);
+                linha_2.appendChild(linha_2_data_2);
+                linha_2.appendChild(linha_2_data_3)
+                tbody.appendChild(linha_2);
+            }
+        }
+            
         )
 
-    .catch (error => console.log(error));
-
-    
-
-
-
-    async function addPecas_ori() {
-
-        const itens = new URLSearchParams()
-        itens.append('codigo', 'teste002')
-        itens.append('nome', 'pecateste')
-        itens.append('fabricante', 'fabricanteste')
-        itens.append('modelo', 'modeloteste')
-        itens.append('linha', 'continuada')
-
-        const url = `http://localhost:3000/api/pecas`
-
-        await axios({
-            method: "post",
-            url: `${url}`,
-            data: itens,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        })
-            .then(response => {
-                alert(response.status)
-            })
-            .catch(error => console.log(error))
-
-    }
-
-    function addPecas_comp() {
-
-    }
-
-    function attPecas_ori() {
-
-    }
-
-    function attPecas_comp() {
-
-    }
-
+        .catch(error => console.log(error));
 
 }
+
+
+
+async function addPecas_ori() {
+
+    const itens = new URLSearchParams()
+    itens.append('codigo', 'teste002')
+    itens.append('nome', 'pecateste')
+    itens.append('fabricante', 'fabricanteste')
+    itens.append('modelo', 'modeloteste')
+    itens.append('linha', 'continuada')
+
+    const url = `http://localhost:3000/api/pecas`
+
+    await axios({
+        method: "post",
+        url: `${url}`,
+        data: itens,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })
+        .then(response => {
+            alert(response.status)
+        })
+        .catch(error => console.log(error))
+
+}
+
+function addPecas_comp() {
+
+}
+
+function attPecas_ori() {
+
+}
+
+function attPecas_comp() {
+
+}
+
+
+
