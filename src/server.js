af2d0fa4f75a0b859b5df7d9b056f3e8c49f1ea5
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-01-14 08:56:59 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
 
 
-server.use('/app/front-end/src',express.static('/app/front-end/src'))
+server.use(express.static('/app/front-end/src'))
 
 server.use('/api',routes)
 
