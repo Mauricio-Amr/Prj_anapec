8af6c61f2a8342e45aeee184e4c35cd537c51e7c
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-01-14 11:58:18 -0300

    teste

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -8,7 +8,7 @@ const routes = require('./routes')
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
-server.use(express.static("/app/front-end/src"))
+server.use(express.static("./front-end/public"))
 server.set('view engine', 'ejs')
 
 server.use('/api',routes)
