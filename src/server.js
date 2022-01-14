14092b6b786e63a39a60e44414b8670c6dfba9a8
Mauricio-Amr <mauricio.s.amr@gmail.com>
2022-01-13 21:54:59 -0300

    render index

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ server.use(bodyParser.urlencoded({extended: false}));
 
 server.use('/api',routes)
 
+server.get('/', function (req,res) {
+    res.render('index.html')
+    
+})
+
 
 server.listen(process.env.PORT, ()=>{
     console.log(`Servidor rodando em : http:localhost:${process.env.PORT}`)
